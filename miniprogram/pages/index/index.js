e0986426f6fdaa83f9bd7f29810e34864e82f14b
sm.huang <649439090@qq.com>
2023-01-06 16:41:56 +0800

    “首页布局修改”

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -2,15 +2,21 @@
 Page({
     data: {
         loading: false,
-        array: ["项目一","项目二","项目三","项目四","项目五","项目六","项目七"]
+        value: 1,
+        array: ["项目一","项目二","项目三","项目四","项目五","项目六","项目七"],
+        resList:[
+            {values:[3,5,23,26,27,32,12], date:"2022-12-12"},
+            {values:[13,15,21,22,27,32,3], date:"2022-12-15"}
+        ]
     },
 
     jumpPage: function(){
-        wx.navigateTo({
-          url: '/pages/search/search',
-        }),
+        // wx.navigateTo({
+        //   url: '/pages/search/search',
+        // }),
         this.setData({
-            loading: true
+            loading: true,
+            value:this.data.value+1
         })
         
     },
